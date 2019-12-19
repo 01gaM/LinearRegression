99184890bd4bb545306bad725feb092ada1dba1c
01gaM <olga.molchanova.olkamana@gmail.com>
2019-12-19 16:23:40 +0300

    Least squares method added
    Signed-off-by: 01gaM <olga.molchanova.olkamana@gmail.com>

--- src/LinearRegression.java
+++ src/LinearRegression.java
@@ -1,6 +1,9 @@
 import org.knowm.xchart.*;
 import org.knowm.xchart.style.Styler;
+import org.knowm.xchart.style.lines.SeriesLines;
 import org.knowm.xchart.style.markers.SeriesMarkers;
+
+import java.awt.*;
 
 public class LinearRegression {
 
@@ -11,15 +14,30 @@ public class LinearRegression {
 
     private double[] noiseDataSetY = new double[dataSetSize]; //y + noise
 
+    private double[] newModelY = new double[dataSetSize]; //y для новой модели линейной регрессии
+    private double a, b; //коэффициенты для линейной аппроксимации вида y = ax + b
 
-    private double f(double x){
+
+    private double f(double x) {
         return (Math.pow(1.1, x) - 2);
     }
 
-    private void generateDataSet(){ //генерирование датасета с добавлением шума
+    private double loss(double yAns, double yRightAns) {//функция потерь
+        return Math.pow(yAns - yRightAns, 2);
+    }
+
+    private double q(double[] yValue) { //функция эмпирического риска
+        double res = 0;
+        for (int i = 0; i < dataSetSize; i++) {
+            res += loss(yValue[i], dataSetY[i]);
+        }
+        return res / dataSetSize;
+    }
+
+    private void generateDataSet() { //генерирование датасета с добавлением шума
         double step = 0.003;
         double currX = 0;
-        for (int i = 0; i < dataSetSize; i++){
+        for (int i = 0; i < dataSetSize; i++) {
             dataSetX[i] = currX;
             dataSetY[i] = f(currX);
 
@@ -31,13 +49,13 @@ public class LinearRegression {
         }
     }
 
-    private void printDataSet(double[] xData, double yData[], int size){
-        for (int i = 0; i < size; i++){
-            System.out.println((i+1) + ") x = " + xData[i] + "; y = " + yData[i]);
+    private void printDataSet(double[] xData, double yData[], int size) {
+        for (int i = 0; i < size; i++) {
+            System.out.println((i + 1) + ") x = " + xData[i] + "; y = " + yData[i]);
         }
     }
 
-    private void showGraph(){
+    private void showGraph() {
         // Create Chart
         XYChart chart = new XYChartBuilder().width(600).height(500).title("Test chart").xAxisTitle("X").yAxisTitle("Y").build();
 
@@ -49,18 +67,56 @@ public class LinearRegression {
 
         // Series
         chart.addSeries("Real value", dataSetX, dataSetY);
-        XYSeries series = chart.addSeries("Value with noise",dataSetX, noiseDataSetY);
+        XYSeries series = chart.addSeries("Value with noise", dataSetX, noiseDataSetY);
+        chart.addSeries("New model value", dataSetX, newModelY);
         series.setMarker(SeriesMarkers.DIAMOND);
 
         new SwingWrapper(chart).displayChart();
     }
 
-    public void solve(){
+    private void leastSquares() {
+        int n = dataSetSize;
+
+        double sumXY = 0;
+        double sumX = 0;
+        double sumY = 0;
+        double sumX2 = 0; //x^2
+
+        for (int i = 0; i < n; i++) {
+            sumX += dataSetX[i];
+            sumY += noiseDataSetY[i];
+            sumXY += dataSetX[i] * noiseDataSetY[i];
+            sumX2 += Math.pow(dataSetX[i], 2);
+        }
+
+        a = (n * sumXY - sumX * sumY) / (n * sumX2 - Math.pow(sumX, 2));
+        b = (sumY - a * sumX) / n;
+    }
+
+    private double linearApproximation(double x, double a, double b){
+        return a*x + b;
+    }
+
+    private void setNewModel(){
+        leastSquares();
+        for (int i = 0; i < dataSetSize; i++){
+            newModelY[i] = linearApproximation(dataSetX[i], a, b);
+        }
+    }
+
+    public void solve() {
         generateDataSet();
         System.out.println("Датасет без шума:");
         printDataSet(dataSetX, dataSetY, dataSetSize);
         System.out.println("Датасет с шумом:");
         printDataSet(dataSetX, noiseDataSetY, dataSetSize);
+
+        System.out.println("\nЗначение функции эмперического риска до МНК: " + q(noiseDataSetY));
+
+        setNewModel();
+
+        System.out.println("Значение функции эмперического риска после МНК: " + q(newModelY));
+
         showGraph();
     }
 
